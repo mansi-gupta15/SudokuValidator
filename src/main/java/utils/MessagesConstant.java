ea7f5d8c15af5dbd6d2f31f0cf0736b28e620b25
Mansi Gupta <mguptapisces@gmail.com>
2021-04-24 16:02:13 +0530

    added gitignore file

---
+++ src/main/java/utils/MessagesConstant.java
@@ -0,0 +1,18 @@
+package utils;
+
+public class MessagesConstant {
+    public static final String NO_FILE = "Error: Please provide 1 filename..";
+    public static final String EMPTY_FILE = "Error: The file is empty";
+    public static final String INVALID_FORMAT = "Error: File is not a valid standard 9x9 Sudoku puzzle";
+    public static final String VALID_FORMAT = "Success: File is a valid standard 9x9 Sudoku puzzle";
+    public static final String NUMERIC_VALIDATION = "Error: Numeric validation failed ";
+    public static final String ROWS_VALIDATION = "Error: The file must have a total of 9 rows";
+    public static final String COLUMN_VALIDATION = "Error: The file must have a total of 9 columns";
+    public static final String VALID_CODE = "0";
+    public static final String INVALID_CODE = "-1";
+    public static final String COMMA_DELIMITER = ",";
+    public static final String FOUND_IN_ROW = " found in row ";
+    public static final String FOUND_IN_COLUMN = " found in column ";
+    public static final String FOUND_IN_SUB_BOX = " found in sub box ";
+
+}
